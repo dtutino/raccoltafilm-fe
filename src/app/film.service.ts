c931bc33f879f761d6edf607cef7ca1575718c05
dtutino <d.tutino@solvingteam.it>
2021-12-09 11:20:57 +0100

    implementato service di prova per Film

---
+++ src/app/film.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Film } from './film';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilmService {
+
+  private apiServer = 'http://localhost:8080/api/film';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private http: HttpClient) { }
+
+  getFilm(idFilmInput: number): Observable<Film> {
+    return this.http.get<Film>(this.apiServer + '/' + idFilmInput.toString()).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+      err.error?.errors?.forEach((element: { message: string; }) => {
+        errorMessage += element.message;
+      });
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+}
+
